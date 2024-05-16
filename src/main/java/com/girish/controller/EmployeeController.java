670f3349f6b174e74b01ec12e66989d52844bad3
GirishKumar3763 <65503614+GirishKumar3763@users.noreply.github.com>
2024-05-16 12:33:55 +0530

    Implementation of Resilence4J and CircuitBreaker

--- src/main/java/com/girish/controller/EmployeeController.java
+++ src/main/java/com/girish/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.girish.controller;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,13 @@ import com.girish.exception.EmployeeNotFoundException;
 import com.girish.model.Employee;
 import com.girish.repository.EmployeeRepository;
 import com.girish.service.EmployeeService;
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 @CrossOrigin
 @RestController
@@ -32,13 +41,21 @@ public class EmployeeController {
 	EmployeeService empService;
 	
 	@GetMapping("/employeebyid/{id}")
+	@RateLimiter(name = "getEmployeeRateLimit" ,fallbackMethod ="getRateLimitFallBack" )
 	public ResponseEntity<Employee> getByEmployeeId(@PathVariable("id") int id) {
 		logger.info("EmployeeController - getByEmployeeId-id:"+id);	
 		Employee emp = empService.getByEmployeeId(id).orElseThrow(() -> new EmployeeNotFoundException("Employee-"+id+" not found with the given ID."));
 		return new ResponseEntity<>(emp, HttpStatus.OK);
 	}
 	
+	public ResponseEntity<String> getRateLimitFallBack(RequestNotPermitted exception) {
+        logger.info("Rate limit has applied, So no further calls are getting accepted");
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+        .body("Too many requests : No further request will be accepted. Please try after sometime");
+    }
+	
 	@GetMapping("/getallemployees")
+	@Retry(name = "getRetryEmployeesList",fallbackMethod = "getRetryEmployeesFallBack")
 	public ResponseEntity<List<Employee>> getAllEmployees() {
 		List<Employee> emps = empService.getAllEmployees();		
 		if (emps.isEmpty()) {
@@ -46,6 +63,12 @@ public class EmployeeController {
 		    }
 
 		return new ResponseEntity<>(emps, HttpStatus.OK);
+	}
+	
+	public ResponseEntity<String> getRetryEmployeesFallBack(Exception e) {
+		logger.info("--RESPONSE FROM FALLBACK METHOD---");
+		
+		return new ResponseEntity<String>("SERVER IS DOWN, PLEASE TRT AFTER SOME TIME", null);
 	}
 
 	@DeleteMapping("/delete/{id}")
@@ -58,18 +81,46 @@ public class EmployeeController {
 	}
 
 	@PostMapping("/insertemployee")
+	@CircuitBreaker(name = "saveEmpCircuitBreaker",fallbackMethod = "saveEmpFallback")
 	public ResponseEntity<Employee> insertEmployee(@RequestBody Employee employee) {
 		Employee emp=empService.addEmployee(employee);
 		return new ResponseEntity<>(emp, HttpStatus.CREATED);
 		
 	}
+	public ResponseEntity<String> saveEmpFallback(Exception e) {
+		logger.info("--RESPONSE FROM FALLBACK METHOD---");
+		
+		return new ResponseEntity<String>("SERVER IS DOWN, PLEASE TRT AFTER SOME TIME", null);
+	}
 
 	@PutMapping("/updateemployee")
+	@TimeLimiter(name = "updateEmpTimeLimitter", fallbackMethod = "updateEmpFallback")
 	public ResponseEntity<Employee>  updateEmployee(@RequestBody Employee employee) {	
 		logger.info("EmployeeController - updateEmployee-id:"+employee.getEmployeeID());	
 		empService.getByEmployeeId(employee.getEmployeeID()).orElseThrow(() -> new EmployeeNotFoundException("Employee-"+employee.getEmployeeID()+" not found with the given ID."));	
 		return new ResponseEntity<>(empService.addEmployee(employee), HttpStatus.OK);	
 	}
+	
+	public ResponseEntity<String> updateEmpFallback(Exception e) {
+		logger.info("--RESPONSE FROM FALLBACK METHOD---");
+		
+		return new ResponseEntity<String>("SERVER IS DOWN, PLEASE TRT AFTER SOME TIME", null);
+	}
+	
+	@GetMapping("/getMessage")
+    @Bulkhead(name = "getMessageBulkHead", fallbackMethod = "getMessageFallBack")
+    public ResponseEntity<String> getMessage(@RequestParam(value="name", defaultValue = "Hello") String name){
+
+       return ResponseEntity.ok().body("Message from getMessage() :" +name);
+    }
+
+    public ResponseEntity<String> getMessageFallBack(RequestNotPermitted exception) {
+
+       logger.info("Bulkhead has applied, So no further calls are getting accepted");
+
+       return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+      .body("Too many requests : No further request will be accepted. Plese try after sometime");
+    }
 	
 	
 }

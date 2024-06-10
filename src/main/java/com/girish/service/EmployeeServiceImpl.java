7c5f670456bb3cddd64fa9479a0fdb383679d878
GirishKumar3763 <65503614+GirishKumar3763@users.noreply.github.com>
2024-06-10 23:17:05 +0530

    Adding ObservationAPI feture

--- src/main/java/com/girish/service/EmployeeServiceImpl.java
+++ src/main/java/com/girish/service/EmployeeServiceImpl.java
@@ -1,8 +1,6 @@
 package com.girish.service;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,19 +8,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
-import com.girish.controller.EmployeeController;
 import com.girish.dto.AddressRecord;
 import com.girish.model.Employee;
 import com.girish.repository.EmployeeRepository;
+
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
 
 
 
@@ -34,7 +30,10 @@ public class EmployeeServiceImpl implements EmployeeService {
 	private static final Logger logger = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 	
 	@Autowired
-	EmployeeRepository employeeRepository;
+	private EmployeeRepository employeeRepository;
+	
+	@Autowired
+	private ObservationRegistry observationRegistry;
 	
 	private final RestTemplate restTemplate;
 	
@@ -44,32 +43,42 @@ public class EmployeeServiceImpl implements EmployeeService {
 	
 	
 	public List<Employee> getAllEmployees() {
-		return (List<Employee>) employeeRepository.findAll();
+		return Observation.createNotStarted("getAllEmployees", observationRegistry)
+				          .observe(()->(List<Employee>) employeeRepository.findAll());
+				
 	}
 
 	
 	public List<Employee> findByEmployeeName(String name) {
-		return (List<Employee>)employeeRepository.findByFirstName(name);
+			
+		return Observation.createNotStarted("findByEmployeeName", observationRegistry)
+		          .observe(()->(List<Employee>)employeeRepository.findByFirstName(name));
+		
 	}
 
 	public Optional<Employee> getByEmployeeId(Integer id){
-	
-		return employeeRepository.findById(id);
+		return Observation.createNotStarted("getByEmployeeId", observationRegistry)
+		          .observe(()->employeeRepository.findById(id));
 	}
 
 	
 	public void deleteEmployee(Integer personId) {
-		employeeRepository.deleteById(personId);
+		
+		Observation.createNotStarted("deleteEmployee", observationRegistry)
+                   .observe(()->employeeRepository.deleteById(personId));
 	}
 
 	
 	public Employee addEmployee(Employee employee) {
-		return employeeRepository.save(employee) ;
+		
+		return Observation.createNotStarted("addEmployee", observationRegistry)
+				          .observe(()->employeeRepository.save(employee));
 	}	
 	
 	public Employee updateEmployee(Employee employee) {
-					
-		return	employeeRepository.save(employee);
+		
+		return Observation.createNotStarted("updateEmployee", observationRegistry)
+		          .observe(()->employeeRepository.save(employee));
 			
 	}
 
